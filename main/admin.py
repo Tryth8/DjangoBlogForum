177aa68a537c2855ea591d0551070df888e88767
Truth <zharylgamysov@gmail.com>
2023-12-25 00:16:43 +0600

    ?

--- main/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-
-# Register your models here.
-from .models import Post, Message, Topic
-
-admin.site.register(Post)
-admin.site.register(Message)
-admin.site.register(Topic)
-
